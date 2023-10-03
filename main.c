a8f96dfb8901401ad65017c9b856bdb0ca171996
Reem sayed <93335879+reemo19@users.noreply.github.com>
2023-10-03 18:49:09 +0300

    main.c

--- main.c
+++ main.c
@@ -10,42 +10,56 @@
 #include "CLCD_interface.h"
 #include "KPD_interface.h"
 #include "TIMER_TEST_interface.h"
+#include "GIE.h"
+#include "TWI.h"
+#include "EEPROM.h"
 #include "SERVO.h"
 #include"util/delay.h"
+#include "avr/interrupt.h"
 #include<avr/io.h>
 
-u8 firstDigit,sDigit,thDigit,lastDigit,pass=0,trypass=0,c=0;
+u8 firstDigit,sDigit,thDigit,lastDigit,pass=0,trypass=0,c=1,x,block;
 int main()
 {
+	//twi
+	TWI_voidMasterInit();
+
 	//led
-	DIO_u8SetPinDir(DIO_PORTD,DIO_PIN1,DIO_PIN_OUT);//G
-	DIO_u8SetPinDir(DIO_PORTD,DIO_PIN2,DIO_PIN_OUT);//R
+	DIO_u8SetPinDir(DIO_PORTC,DIO_PIN2,DIO_PIN_OUT);//G
+	DIO_u8SetPinDir(DIO_PORTC,DIO_PIN3,DIO_PIN_OUT);//R
 
 	//SERVO
 	 DIO_u8SetPinDir(DIO_PORTD,DIO_PIN5,DIO_PIN_OUT);
-	 //TIMER
+	 //TIMER 1
 	TIMER1_voidInit();
 	TIMER1_voidSetICR(20000);
+	//TIMER 0
+
+	TIMER0_voidOfInit();
+	TIMER0_voidPreLoadValue(192);
+	GIE_voidEnable();
 	//LCD
 		  DIO_u8SetPortDir(DIO_PORTA, DIO_PORT_OUT);
-		  DIO_u8SetPortDir(DIO_PORTB, 0x03);
+		  DIO_u8SetPinDir(DIO_PORTC,DIO_PIN5,DIO_PIN_OUT);
+		  DIO_u8SetPinDir(DIO_PORTC,DIO_PIN6,DIO_PIN_OUT);
 			//kpd
-			DIO_u8SetPortDir(DIO_PORTC, 0b00000111);
-			DIO_u8SetPortValue(DIO_PORTC, 0xff);
+			DIO_u8SetPortDir(DIO_PORTB, 0b00000111);
+			DIO_u8SetPortValue(DIO_PORTB, 0xff);
 			CLCD_voidInit();
 	//var
 u8 value;
 CLCD_voidSendString("THE SAFE SYSTEM");
 CLCD_voidGoToXY(1,0);
 CLCD_voidSendString("enter '*' to start");
+c=1;
 	while(1)
 	{
-		if(c==0)
+
+		 x=EEPROM_read(0x111);
+		block= EEPROM_read(0x100);
+		if(x==0xff)
 		{
-		  do{
-		   value = KPD_u8GetPressedKey();
-		    	 }while (value == 0xff);
-              if(value==10){
+
      				CLCD_voidSendCommand(0x01);
      				CLCD_voidSendString("password=----");
      				CLCD_voidGoToXY(0,9);
@@ -69,7 +83,7 @@ CLCD_voidSendString("enter '*' to start");
  					_delay_ms(300);
  					CLCD_voidGoToXY(0,10);
  					CLCD_voidSendData('*');
- 					//hours_counter=(second_digit-48)+10*(first_digit-48);
+ 				
  				CLCD_voidGoToXY(0,11);
  				do
        			{
@@ -91,50 +105,46 @@ CLCD_voidSendString("enter '*' to start");
        			_delay_ms(300);
        			CLCD_voidGoToXY(0,12);
        			CLCD_voidSendData('*');
-       			c++;
               pass=(lastDigit)+1000*(thDigit)+100*(sDigit)+10*(firstDigit);
+              EEPROM_write(0x111,pass);
+              	_delay_ms(100);
+              x=EEPROM_read(0x111);
+
               CLCD_voidSendCommand(0x01);
-	}
+
 		}
-		else if(c<4)
+		else if(x!=0xff&&c<4)
 		{
-
-			CLCD_voidSendString("press '*' to ");
-            CLCD_voidGoToXY(1,0);
-            CLCD_voidSendString("enter password");
-            do{
-           					   value = KPD_u8GetPressedKey();
-           					    	 }while (value == 0xff);
-           			              if(value==10){
-			 do{
-					   value = KPD_u8GetPressedKey();
-			    	 }while (value == 0xff);
-
-	     				CLCD_voidSendCommand(0x01);
-
-	     				CLCD_voidSendString("password");
-	     				CLCD_voidWriteNumber(c);
-	     				CLCD_voidSendString("=----");
-	     				CLCD_voidGoToXY(0,10);
+			  CLCD_voidSendCommand(0x01);
+			CLCD_voidSendString("check pass");
+			_delay_ms(150);
+                 CLCD_voidSendCommand(0x01);
+                 CLCD_voidGoToXY(0,0);
+                  CLCD_voidSendString("pass");
+	     		CLCD_voidWriteNumber(c);
+	     		CLCD_voidSendString("=----");
+	     	   CLCD_voidGoToXY(0,6);
 	     				_delay_ms(500);
 	     				do
 	     				{
+
 	     					firstDigit=KPD_u8GetPressedKey();
 
 	     				} while (firstDigit==KPD_NO_PRESSED_KEY);
+	     				CLCD_voidGoToXY(0,6);
 	     				CLCD_voidWriteNumber(firstDigit);
 	     				_delay_ms(300);
-	     				CLCD_voidGoToXY(0,10);
+	     				CLCD_voidGoToXY(0,6);
 	     				CLCD_voidSendData('*');
 	     				do
 	 					{
 	 						sDigit=KPD_u8GetPressedKey();
 
 	 					} while (sDigit==KPD_NO_PRESSED_KEY);
-	     				CLCD_voidGoToXY(0,11);
+	     				CLCD_voidGoToXY(0,7);
 	 					CLCD_voidWriteNumber(sDigit);
 	 					_delay_ms(300);
-	 					CLCD_voidGoToXY(0,11);
+	 					CLCD_voidGoToXY(0,7);
 	 					CLCD_voidSendData('*');
 
 	 				do
@@ -142,42 +152,44 @@ CLCD_voidSendString("enter '*' to start");
 	       				thDigit=KPD_u8GetPressedKey();
 
 	       			} while (thDigit==KPD_NO_PRESSED_KEY);
-	 				CLCD_voidGoToXY(0,12);
+	 				CLCD_voidGoToXY(0,8);
 	       			CLCD_voidWriteNumber(thDigit);
 	       			_delay_ms(300);
-	       			CLCD_voidGoToXY(0,12);
+	       			CLCD_voidGoToXY(0,8);
 	       			CLCD_voidSendData('*');
 	       			do
 	       			{
 	       				lastDigit=KPD_u8GetPressedKey();
 
 	       			} while (lastDigit==KPD_NO_PRESSED_KEY);
-	       			CLCD_voidGoToXY(0,13);
+	       			CLCD_voidGoToXY(0,9);
 	       			CLCD_voidWriteNumber(lastDigit);
 	       			_delay_ms(300);
-	       			CLCD_voidGoToXY(0,13);
+	       			CLCD_voidGoToXY(0,9);
 	       			CLCD_voidSendData('*');
 	       			c++;
 	             trypass=(lastDigit)+1000*(thDigit)+100*(sDigit)+10*(firstDigit);
 	              CLCD_voidSendCommand(0x01);
 
-	              		if(pass==trypass)
+	              		if(x==trypass)
 	              		{
+
 	              			SERVO_voidSendAngle(500);
 
-	              				DIO_u8SetPinValue(DIO_PORTD,DIO_PIN1,DIO_HIGH);
-	              				DIO_u8SetPinValue(DIO_PORTD,DIO_PIN2,DIO_LOW);
+	              				DIO_u8SetPinValue(DIO_PORTC,DIO_PIN2,DIO_HIGH);
+	              				DIO_u8SetPinValue(DIO_PORTC,DIO_PIN3,DIO_LOW);
 
 	              				c=4;
 	              				CLCD_voidSendCommand(0x01);
 	              				CLCD_voidSendString("open ^_^");
-	              			}
+	              				CLCD_voidGoToXY(1,0);
+	              		}
 	              			else
 	              			{
 
 	              				//led
-	              				DIO_u8SetPinValue(DIO_PORTD,DIO_PIN1,DIO_LOW);
-	              				DIO_u8SetPinValue(DIO_PORTD,DIO_PIN2,DIO_HIGH);
+	              				DIO_u8SetPinValue(DIO_PORTC,DIO_PIN2,DIO_LOW);
+	              				DIO_u8SetPinValue(DIO_PORTC,DIO_PIN3,DIO_HIGH);
 	              				CLCD_voidSendCommand(0x01);
 	              				if(c<4)
 	              				{
@@ -191,21 +203,47 @@ CLCD_voidSendString("enter '*' to start");
 	    				         }
 	              				else
 	              				{
+
 	              				CLCD_voidSendString("wrong password");
 	              				CLCD_voidGoToXY(1,7);
 	              				CLCD_voidSendString("-_-");
 	              				DIO_u8SetPinDir(DIO_PORTB,DIO_PIN7,DIO_PIN_OUT);
 	              				 SET_BIT(PORTB,7);
- 	              			   _delay_ms(500);
-
+	              				EEPROM_write(0x100,0x00);
+	              				block= EEPROM_read(0x100);
+ 	              			   _delay_ms(150);
+                                 c=1;
 	              				}
 
 	              		}
-			              }
+
 		}
 
 
 		}
 	}
+ISR(TIMER0_OVF_vect)
+{
+	static u16 a=0;
+	a++;
+	if(a==3907){
+      TCNT0=192;
+      a=0;
+      if(block!=0xff){
+	   // CLCD_voidSendCommand(0x01);
+	        CLCD_voidGoToXY(1,0);
+	    	CLCD_voidSendString("wait 30s!!!");
+	    	DIO_u8SetPinDir(DIO_PORTB,DIO_PIN7,DIO_PIN_IN);
+	    	 CLEAR_BIT(PORTB,7);
+	    	_delay_ms(3000);
+	    	 c=1;
+	    	EEPROM_write(0x100,0xff);
+	      block= EEPROM_read(0x100);
+	     x=EEPROM_read(0x111);
+	    	 CLCD_voidGoToXY(1,0);
+	    	 CLCD_voidSendString("try know");
 
+      }
 
+	    }
+	}
